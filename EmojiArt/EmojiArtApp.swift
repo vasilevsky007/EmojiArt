61ab813d5575f6b40e99d44af52d01c1e07eb31b
alex <alex.vasilevski007@gmail.com>
2023-03-15 19:09:38 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Alex on 15.03.23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
